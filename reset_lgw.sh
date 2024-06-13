7a3fd69cdfeebc0f04e0c656937831f3e565d380
KressAlex <40684289+KressAlex@users.noreply.github.com>
2024-06-13 12:20:36 -0600

    Create reset_lgw.sh

---
+++ reset_lgw.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# This script is intended to be used on SX1302 CoreCell platform, it performs
+# the following actions:
+#       - export/unpexort GPIO23 and GPIO18 used to reset the SX1302 chip and to enable the LDOs
+#
+# Usage examples:
+#       ./reset_lgw.sh stop
+#       ./reset_lgw.sh start
+
+# GPIO mapping has to be adapted with HW
+#
+
+SX1302_RESET_PIN=25
+
+WAIT_GPIO() {
+    sleep 0.1
+}
+
+init() {
+    # setup GPIOs
+ #   echo "$SX1302_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
+
+    # set GPIOs as output
+ #   echo "out" > /sys/class/gpio/gpio$SX1302_RESET_PIN/direction; WAIT_GPIO
+
+raspi-gpio set 25 op
+}
+
+reset() {
+    echo "CoreCell reset through GPIO$SX1302_RESET_PIN..."
+
+        raspi-gpio set 25 op dh
+        sleep 0.1
+        raspi-gpio set 25 op dl
+        sleep 0.1    
+        #echo "1" > /sys/class/gpio/gpio$SX1302_RESET_PIN/value; WAIT_GPIO
+    #echo "0" > /sys/class/gpio/gpio$SX1302_RESET_PIN/value; WAIT_GPIO
+}
+
+term() {
+    # cleanup all GPIOs
+    if [ -d /sys/class/gpio/gpio$SX1302_RESET_PIN ]
+    then
+        echo "$SX1302_RESET_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
+    fi
+}
+
+case "$1" in
+    start)
+    term # just in case
+
